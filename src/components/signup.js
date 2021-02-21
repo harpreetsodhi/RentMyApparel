09cea5678c210ecbd9a5f1e95baa514855ca9ba6
neelkanth <nl543396@dal.ca>
2021-02-21 18:54:15 -0400

    added login and sign up

---
+++ src/components/signup.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+
+export default class SignUp extends Component {
+  render() {
+    return (
+      <form>
+        <h3 className="text-center">Sign Up</h3>
+
+        <div className="form-group">
+          <label>User Name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Username"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+        <div className="form-group">
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Re-enter password"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary btn-block">
+          Sign Up
+        </button>
+
+        <div class="text-center">
+          <p class="m-2">
+            Already a member? <a href="#">Sign In</a>
+          </p>
+          <p>or Sign In with:</p>
+          <button type="button" class="btn btn-danger btn-circle  m-1">
+            <i class="fa fa-google"></i>
+          </button>
+          <button type="button" class="btn btn-info btn-circle  m-1">
+            <i class="fa fa-twitter"></i>
+          </button>
+          <button type="button" class="btn btn-dark btn-circle m-1">
+            <i class="fa fa-github"></i>
+          </button>
+          <button type="button" class="btn btn-primary btn-circle  m-1">
+            <i class="fa fa-facebook"></i>
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,65 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './Header';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Header";
+import Login from "./components/login.js";
+import SignUp from "./components/signup.js";
+import "font-awesome/css/font-awesome.min.css";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
-class Index extends Component{
-    render = () => {
-        return (
-            <Header></Header>
-        );
-    }
+class Index extends Component {
+  // https://stackoverflow.com/questions/46845543/react-bootstrap-tab-not-changing-content
+  constructor(props) {
+    super(props);
+    this.state = {
+      key: 1,
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  handleSelect(key) {
+    console.log("selected" + key);
+    this.setState({ key: key });
+  }
+
+  render = () => {
+    return (
+      <div>
+        <Header></Header>
+
+        <div
+          class="rows justify-content-center"
+          style={{
+            width: "20%",
+            margin: "auto",
+            marginTop: "3%",
+          }}
+        >
+          <Tabs
+            class="nav nav-pills justify-content-center"
+            activeKey={this.state.key}
+            onSelect={this.handleSelect}
+            id="tabs"
+            style={{
+              marginBottom: "5%",
+            }}
+          >
+            <Tab eventKey={1} title="Sign In">
+              <div>
+                <Login />
+              </div>
+            </Tab>
+            <Tab eventKey={2} title="Sign Up">
+              <div>
+                <SignUp />
+              </div>
+            </Tab>
+          </Tabs>
+        </div>
+      </div>
+    );
+  };
 }
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(<Index />, document.getElementById("root"));
