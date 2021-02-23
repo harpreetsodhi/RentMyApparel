a6eee72aada26839c0807ffb4128c2788227d7e6
neelkanth <nl543396@dal.ca>
2021-02-23 13:53:05 -0400

    fixed login signup pages

--- src/index.js
+++ src/index.js
@@ -8,13 +8,8 @@ import "font-awesome/css/font-awesome.min.css";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import Thrift from "./components/thrift.js";
-import Contact from "./components/contact.js"
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Contact from "./components/contact.js";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 class Index extends Component {
   // https://stackoverflow.com/questions/46845543/react-bootstrap-tab-not-changing-content
@@ -39,46 +34,6 @@ class Index extends Component {
           <Route path="/login" exact component={Login}></Route>
           <Route path="/signup" exact component={SignUp}></Route>
         </BrowserRouter>
-
-        <div
-          class="rows justify-content-center"
-          style={{
-            width: "20%",
-            margin: "auto",
-            marginTop: "3%",
-          }}
-        >
-          <Tabs
-            class="nav nav-pills justify-content-center"
-            activeKey={this.state.key}
-            onSelect={this.handleSelect}
-            id="tabs"
-            style={{
-              marginBottom: "5%",
-            }}
-          >
-            <Tab eventKey={1} title="Sign In">
-              <div>
-                <Login />
-              </div>
-            </Tab>
-            <Tab eventKey={2} title="Sign Up">
-              <div>
-                <SignUp />
-              </div>
-            </Tab>
-            <Tab eventKey={3} title="Thrift Store">
-              <div>
-                <Thrift />
-              </div>
-            </Tab>
-            <Tab eventKey={4} title="Contact Us">
-              <div>
-                <Contact />
-              </div>
-            </Tab>
-          </Tabs>
-        </div>
       </div>
     );
   };
