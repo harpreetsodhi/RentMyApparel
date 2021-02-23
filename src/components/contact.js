f4753586b95c8cc76840dd3eb534e0c0480b20a5
neelkanth <nl543396@dal.ca>
2021-02-23 14:18:16 -0400

    save point

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,42 +1,34 @@
 import React from "react";
-import styles from "../contact.css"
+import styles from "../contact.css";
 const Contact = () => {
-return (
-    <form >
-        <div class="p-5 text-center bg-contact-image" style={{ backgroundImage: "url(maps.jpg)" }} >
-        <h3 className="text-center" >
-                    Contact Us
-        </h3>
+  return (
+    <form>
+      <div
+        class="p-5 text-center bg-contact-image"
+        style={{ backgroundImage: "url(maps.jpg)" }}
+      >
+        <h3 className="text-center">Contact Us</h3>
         <div className="form-group">
-        <label>Name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Name"/>
+          <label>Name</label>
+          <input type="text" className="form-control" placeholder="Name" />
         </div>
 
         <div className="form-group">
-        <label>Email Address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Email"/>
+          <label>Email Address</label>
+          <input type="email" className="form-control" placeholder="Email" />
         </div>
 
         <div className="form-group">
-        <label>Message</label>
-          <input
-            rows="3"
-            className="form-control"
-            placeholder="Message"/>
+          <label>Message</label>
+          <input rows="3" className="form-control" placeholder="Message" />
         </div>
 
         <button type="submit" className="btn btn-dark">
           Contact Us
         </button>
-        </div>
-</form>  
-)
-}
+      </div>
+    </form>
+  );
+};
 
 export default Contact;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Header from "./Header";
 import Login from "./components/login.js";
 import SignUp from "./components/signup.js";
 import Products from "./components/products.js";
+import Home from "./components/home.js";
 import "font-awesome/css/font-awesome.min.css";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
@@ -32,6 +33,7 @@ class Index extends Component {
       <div>
         <Header></Header>
         <BrowserRouter>
+          <Route path="/" exact component={Home}></Route>
           <Route path="/login" exact component={Login}></Route>
           <Route path="/signup" exact component={SignUp}></Route>
           <Route path="/products" exact component={Products}></Route>
