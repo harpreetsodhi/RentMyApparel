4e0734be4abc8906fe5af2c79b6c15e48ce86834
neelkanth <nl543396@dal.ca>
2021-02-23 14:01:49 -0400

    added products page to nav

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,12 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { Button, Navbar, Nav, Form, FormControl } from "react-bootstrap";
 import styles from "./style.module.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class Header extends Component {
   constructor(props) {
@@ -28,9 +23,14 @@ class Header extends Component {
           <Nav.Link style={{ color: "white" }} href="/signup">
             Sign Up
           </Nav.Link>
+          <Nav.Link style={{ color: "white" }} href="/products">
+            Products
+          </Nav.Link>
         </Nav>
         <Form inline>
-          <Nav.Link style={{color : "white"}} href="#contact">Contact Us</Nav.Link>
+          <Nav.Link style={{ color: "white" }} href="#contact">
+            Contact Us
+          </Nav.Link>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-light">Search</Button>
         </Form>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Header";
 import Login from "./components/login.js";
 import SignUp from "./components/signup.js";
+import Products from "./components/products.js";
 import "font-awesome/css/font-awesome.min.css";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
@@ -33,6 +34,7 @@ class Index extends Component {
         <BrowserRouter>
           <Route path="/login" exact component={Login}></Route>
           <Route path="/signup" exact component={SignUp}></Route>
+          <Route path="/products" exact component={Products}></Route>
         </BrowserRouter>
       </div>
     );
