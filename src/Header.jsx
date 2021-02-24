d8dd7dd54631a8d877bca21a913e99439795ee23
Rajveen <17948747+rajveen@users.noreply.github.com>
2021-02-23 21:38:20 -0400

    Changed position of account icon

--- src/Header.jsx
+++ src/Header.jsx
@@ -43,20 +43,6 @@ class Header extends Component {
                   marginLeft: "10px",
                   marginRight: "10px",
                 }}
-                href="/account"
-              >
-                {/* https://icons.getbootstrap.com/icons/person-circle/ */}
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                  <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                </svg>
-              </Nav.Link>
-              <Nav.Link
-                style={{
-                  color: "#EEEEEE",
-                  marginLeft: "10px",
-                  marginRight: "10px",
-                }}
                 href="/login"
               >
                 Login
@@ -80,6 +66,20 @@ class Header extends Component {
                 href="/contact"
               >
                 Contact Us
+              </Nav.Link>
+              <Nav.Link
+                style={{
+                  color: "#EEEEEE",
+                  marginLeft: "10px",
+                  marginRight: "10px",
+                }}
+                href="/account"
+              >
+                {/* https://icons.getbootstrap.com/icons/person-circle/ */}
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                  <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                </svg>
               </Nav.Link>
             </Nav>
           </div>
