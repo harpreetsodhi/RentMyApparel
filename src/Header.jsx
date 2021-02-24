6143273997a85fe58ee8307574b9b1193b698452
neelkanth <nl543396@dal.ca>
2021-02-23 23:08:34 -0400

    fixed user logo to end

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { Button, Navbar, Nav, Form, FormControl } from "react-bootstrap";
 import styles from "./style.module.css";
-import { BrowserRouter as Router, Switch, Route, Link, NavLink } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
 
 class Header extends Component {
   constructor(props) {
@@ -73,13 +79,9 @@ class Header extends Component {
                   marginLeft: "10px",
                   marginRight: "10px",
                 }}
-                href="/account"
+                href="/Cart"
               >
-                {/* https://icons.getbootstrap.com/icons/person-circle/ */}
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                  <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                </svg>
+                Cart
               </Nav.Link>
               <Nav.Link
                 style={{
@@ -87,9 +89,23 @@ class Header extends Component {
                   marginLeft: "10px",
                   marginRight: "10px",
                 }}
-                href="/Cart"
+                href="/account"
               >
-                Cart
+                {/* https://icons.getbootstrap.com/icons/person-circle/ */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  className="bi bi-person-circle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                  <path
+                    fillRule="evenodd"
+                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                  />
+                </svg>
               </Nav.Link>
             </Nav>
           </div>
