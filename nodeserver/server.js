7cd715549ab0407e5dca3269d708fe74ba7ae967
neelkanth <nl543396@dal.ca>
2021-03-20 14:40:54 -0300

    fixed mongo connection issue , added api for signup

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcryptjs')
+const { check, validationResult} = require("express-validator/check");
+const jwt = require('jsonwebtoken')
 const app = express()
 app.use(express.json())
 const mongoose = require("mongoose")
@@ -7,14 +11,16 @@ const port = process.env.PORT || 5000;
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const userModel = require("./model/userModel")
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use(express.json());
 app.use("/", router);
 // app.listen(5000, () => console.log("Server Running"));
 
 mongoose
-	.connect("mongodb+srv://Shivani0109:Shivani1998@cluster0.jxphq.mongodb.net", { useNewUrlParser: true,  useUnifiedTopology: true })
+	.connect("mongodb+srv://Shivani0109:Shivani1998@cluster0.jxphq.mongodb.net/RentmyApparel?retryWrites=true&w=majority", { useNewUrlParser: true,  useUnifiedTopology: true })
 	.then(() => {
 
 		app.get('/', (req, res) => {
@@ -70,4 +76,79 @@ const contactEmail = {
       }
     });
   });
+
+router.post(
+    "/signup",
+    [
+        check("user_id", "Please Enter a Valid User Name").not().isEmpty(),
+        check("user_name", "Please Enter a Valid Username")
+            .not()
+            .isEmpty(),
+        check("user_email", "Please enter a valid email").isEmail(),
+        check("user_password", "Please enter a valid password").isLength({
+            min: 6
+        })
+    ],
+
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        const {
+            user_id,
+            user_name,
+            user_email,
+            user_password,
+        } = req.body;
+        try {
+            let user = await userModel.findOne({
+                user_email
+            });
+            if (user) {
+                return res.status(400).json({
+                    msg: "User Already Exists"
+                });
+            }
+            user = new userModel({
+                user_id,
+                user_name,
+                user_email,
+                user_password
+            });
+
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(user_password, salt);
+
+            await user.save();
+
+            const payload = {
+                user: {
+                    user_id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                "randomString", {
+                    expiresIn: 10000
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.status(200).json({
+                        token
+                    });
+                }
+            );
+        } catch (err) {
+            console.log(err.message);
+            res.status(500).send("Error in Saving");
+        }
+    }
+);
+
+
 
