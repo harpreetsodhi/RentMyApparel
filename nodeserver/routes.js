898a5eadebe5069189b2de1f824b8b9ef5df028a
Harpreet Singh <hr557871@dal.ca>
2021-03-20 11:39:32 -0300

    added gitignore

---
+++ nodeserver/routes.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const Cart = require("./model/cartModel")
+const router = express.Router()
+
+router.get("/cart", async (req, res) => {
+	const items = await Cart.find()
+	res.send(items)
+})
+
+
+module.exports = router
