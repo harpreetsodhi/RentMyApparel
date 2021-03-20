135d7c72448524bca8931bef1f8f402a3ba865dd
Shivani Sharma <Sh477091@dal.ca>
2021-03-20 15:10:19 -0300

    CHnages

--- nodeserver/routes.js
+++ nodeserver/routes.js
@@ -8,31 +8,7 @@ const router = express.Router()
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.get('/products',  (req,res)=>{
-    productModel.find()
-    .exec()
-    .then(result => {
-        console.log("Result: " + result);
-        res.status(200).json({message: "Fetching ...", result});
-    })
-});
 
-router.get('/singleproduct/:productId', (req,res)=>{
-    try{
-        let product_id = req.params.productId;
-        
-        searchProduct("product_id", product_id).exec()
-        .then(result => {
-            console.log(result);
-            res.status(200).json({message: result});
-        })
-    }catch(error){
-        console.log(error.message);
-        res.status(404).json({
-            message: error.message
-        })
-    }
-});
 
 router.get("/cart", async (req, res) => {
 	const items = await Cart.find()
