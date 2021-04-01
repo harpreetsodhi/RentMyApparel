bc7d3528786aa083a3455b80ef8899db4d0bbf33
Shivani Sharma <Sh477091@dal.ca>
2021-04-01 00:52:20 -0300

    search filter

--- src/components/thrift.js
+++ src/components/thrift.js
@@ -17,15 +17,17 @@ class Thrift extends React.Component {
     this.state={
       products: [], 
       users:[],
+      searchResults: []
     }
   }
 
   //search a component//
-  searchProduct = (product_title) => {
+  searchProduct = (e) => {
+    let product_title = e.target.value;
     axios.get("https://rent-my-apparel-backend.herokuapp.com/api/products/title/"+product_title).then(
       res=>{
         console.log(res);
-        this.setState({products: this.state.products})
+        this.setState({searchResults: [res.data]})
       }
     ).catch(err=>console.log)
   }
@@ -46,10 +48,59 @@ class Thrift extends React.Component {
           <strong> Thrift Store</strong>  
           </a>
           <form class="form-inline">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={this.searchProduct.bind(this)} />
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={this.searchProduct} />
           </form>
         </nav>
 
+        <div className="card-deck">
+          <h3>Search Results:</h3>
+          <CardColumns>
+            {
+              this.state.searchResults.map(product => {
+                return(
+                  <Card>
+                    <Card.Body>
+                      <Card.Img
+                        class="card-img-top"
+                        variant="top"
+                        src={product.product_img}
+                      />
+                      <Card.Text className="text-center">
+                        <a href="#">
+                        {product.product_title}
+                        </a>
+                      </Card.Text>
+                      <Card.Text className="text-center">
+                        Product Color - {product.product_color}
+                      </Card.Text>
+                      <Card.Text className="description">
+                          {product.product_desc}
+                      </Card.Text>  
+                      <Card.Footer>
+                      <Card.Subtitle className="text-center">Price: ${product.product_price}</Card.Subtitle>
+                      </Card.Footer>
+                      <Card.Footer>
+                      <Card.Subtitle className="text-center">Size: {product.product_size}
+                      </Card.Subtitle>
+                      </Card.Footer>
+                      <Card.Footer>
+                        <div className="col text-center">
+                        <Button variant="btn btn-outline-success" >Add to Cart</Button>
+                        </div>
+                        <br />
+                        <div className="col text-center">
+                        </div>
+                      </Card.Footer>
+                    </Card.Body>
+                  </Card>
+
+                );
+              })
+            }
+            
+          </CardColumns>
+        </div>
+        <hr className="mb-5 mt-5" />
         <div className="card-deck">
           <CardColumns>
             {
