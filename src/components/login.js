5cfbbdf96d07883c37b9a524a2d090056bbf35a0
neelkanth <nl543396@dal.ca>
2021-04-02 15:15:50 -0300

    save changes

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,8 @@
 import React, { Component, useState, browserHistory } from "react";
 import axios from "axios";
 import Alert from "react-bootstrap/Alert";
+import {  isUserAccountComplete  } from "../helper/functions";
+
 
 // AUTHOR : NEELKANTH DABHI
 //  Ref: https://www.positronx.io/build-react-login-sign-up-ui-template-with-bootstrap-4/
@@ -50,7 +52,12 @@ const Login = () => {
               "user_account_completeness",
               response["data"]["isComplete"]
             );
-            window.location.replace("/account");
+            console.log(isUserAccountComplete())
+            if (isUserAccountComplete()){
+              window.location.replace('/')
+            }else {
+              window.location.replace('/account')
+            }
           }
         })
         .catch(function (error) {
